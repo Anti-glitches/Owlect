e50e8e94c523e7a7b10899c8f7f35faa63bd172b
Danial Aiman Jamil <58195654+KawakazeRin@users.noreply.github.com>
2022-11-05 02:16:34 +0800

    Icon & Topic Route

---
+++ src/views/TopicView.vue
@@ -0,0 +1,204 @@
+<template>
+    <div>
+        <!-- Topic Hero -->
+        <div class="flex bg-green-100 p-12 gap-10 flex-col items-start">
+            <button>
+                <i class="fa-solid fa-chevron-left"></i> Back to Course
+            </button>
+            <div class="flex gap-12">
+                <div class="flex flex-col gap-5">
+                    <h1 class="text-3xl">{{topic.name}}</h1>
+                    <p>
+                        {{topic.description}}
+                    </p>
+                </div>
+                <div
+                    class="
+                        border
+                        bg-blue-200
+                        flex flex-col
+                        gap-3
+                        flex-shrink-0
+                        border-black
+                        w-96
+                        h-60
+                    "
+                >
+                    <div class="w-full h-full">video here</div>
+                    <div class="flex justify-between bg-white p-4">
+                        <p>What’s Human Anatomy?</p>
+                        <span><i class="fa-solid fa-play"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Mid content -->
+        <div class="p-12 flex flex-col gap-6">
+            <h1 class="text-2xl">Human's Nose</h1>
+            <div class="flex gap-10">
+                <div
+                    class="
+                        border
+                        bg-blue-200
+                        flex flex-col
+                        gap-3
+                        flex-shrink-0
+                        border-black
+                        w-96
+                        h-60
+                    "
+                ></div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Explicabo corrupti, molestiae impedit tempora expedita
+                    deleniti eveniet eum, soluta consequuntur commodi ab quae
+                    eos fugiat autem? Fuga explicabo fugiat ratione natus
+                    voluptate sunt voluptatibus iure porro iste? Ullam aliquid
+                    repudiandae deserunt.
+                </p>
+            </div>
+        </div>
+
+        <!-- Project Section -->
+        <div class="p-12 flex flex-col gap-6">
+            <h1 class="text-3xl">Project Time!</h1>
+            <p>
+                Record your explanation about the following topic, and good
+                luck!
+            </p>
+
+            <p class="bg-yellow-300 p-6 text-xl text-center rounded">
+                "Things that happen in a human’s nose and its anatomy"
+            </p>
+
+            <div class="flex gap-12">
+                <div class="flex flex-col gap-4">
+                    <h2 class="text-2xl">Instructions</h2>
+                    <p>
+                        Lörem ipsum framtidsfullmakt. Delig mms:a om räsk. Plav
+                        bevis egor övis. Timåpär riviligen. Värdgraviditet
+                        metataggar med por därför att fav. Debel öv poddsändning
+                        branded content. Parask pararen för att kos. Jimyre
+                        mutina. Varat sor. sjsanxaizlao aiosunoais aoiyxbayxna
+                        ainxaoixnas xanyxax aybuois xansb
+                    </p>
+                    <p>In your explanation, you need to mention:</p>
+                    <ul>
+                        <li class="list-outside">XXXX</li>
+                        <li class="list-outside">XXXX</li>
+                        <li class="list-outside">XXXX</li>
+                    </ul>
+                    <p>
+                        Debel öv poddsändning branded content. Parask pararen
+                        för att kos. Jimyre mutina. Varat sor. sjsanxaizlao
+                        aiosunoais aoiyxbayxna ainxaoixnas xanyxax aybuois xansb
+                    </p>
+                </div>
+                <div
+                    class="
+                        border
+                        bg-blue-200
+                        flex flex-col
+                        gap-3
+                        flex-shrink-0
+                        border-black
+                        w-96
+                        h-60
+                    "
+                >
+                    <p>Meet the Tutors!</p>
+                </div>
+            </div>
+
+            <div class="grid grid-cols-3 gap-8">
+                <div class="p-6 border shadow flex flex-col gap-3">
+                    <h1 class="text-2xl font-bold">Feedback</h1>
+                    <div>
+                        <p>Understanding</p>
+                        <p>-</p>
+                    </div>
+                    <div>
+                        <p>Understanding</p>
+                        <p>-</p>
+                    </div>
+                    <div>
+                        <p>Understanding</p>
+                        <p>-</p>
+                    </div>
+
+                    <div class="text-right">
+                        <p class="p-2 bg-yellow-200 rounded">Overall</p>
+                        <p class="p-2">-</p>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-6 col-span-2">
+                    <div
+                        class="
+                            border-2 border-dashed border-indigo-400
+                            rounded
+                            h-full
+                            text-center
+                            flex flex-col
+                            justify-center
+                        "
+                    >
+                        <i
+                            class="
+                                fa-solid fa-cloud-arrow-up
+                                text-6xl text-indigo-300
+                            "
+                        ></i>
+                        <p>Upload Files</p>
+                        <p>suitable extensions: .mp4, .mp3</p>
+                    </div>
+                    <div class="flex justify-between items-center">
+                        <p>Not Uploaded</p>
+                        <button class="p-2 px-6 bg-red-400 rounded text-white">
+                            Submit <i class="fa-solid fa-chevron-right"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router';
+export default{
+    data() {
+        return {
+            topic: []
+        };
+    },
+    mounted() {
+        const query = `{
+		    topicsCollection(where: {slug: "${this.$route.params.topicName}"}){
+		        items{
+			        name
+                    description
+		        }
+	        }
+        }
+         `;
+        const getData = async () => {
+            const res = await fetch("https://graphql.contentful.com/content/v1/spaces/h7anfqe067rx/", {
+                method: "POST",
+                headers: {
+                    Authorization: "Bearer tADicLUUI2k4He69iAlp8jrF-n-4LJrf60S3UJr_uJs",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ query }),
+            }).then(res => res.json()).then(data => {
+                this.topic = data.data.topicsCollection.items[0];
+            });
+        };
+        return getData();
+    },
+    components: { RouterLink }
+}
+</script>
+
+<style>
+</style>
