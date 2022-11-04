0490c53ec8ed90e7128f38d417108b04aea15966
Ahmad Fahrezi <76411744+Anti-glitches@users.noreply.github.com>
2022-11-05 02:18:35 +0800

    teacher dashboard

--- src/views/Teacher/TDashboardView.vue
+++ src/views/Teacher/TDashboardView.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>teacher</div>
+    <div class="p-12">
+        <div class="border p-8 flex flex-col gap-3">
+            <h1 class="text-2xl font-medium">Hello, Ms. Tatcher!</h1>
+            <p class="text-xl">
+                Here’s some recap on projects you have reviewed this week:
+            </p>
+            <div>
+                <div>
+                    <h3>Sunday</h3>
+                    <p>26</p>
+                    <p>projects</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
